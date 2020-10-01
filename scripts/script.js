af2e5da484fe0048e4bc18e17864dec52a1d98c1
theart84 <artshadow74@gmail.com>
2020-10-01 16:28:32 +0500

    Сделано переключение размера шрифта.

--- scripts/script.js
+++ scripts/script.js
@@ -2,27 +2,20 @@
 function changeStandart() {
     let btnArray = document.getElementsByClassName('spacial-panel-long-block-button__element');
     let newBtnArray = [...btnArray];
-    let bodyHasClassBlue = $('body').hasClass('invert-blue');
-    let bodyHasClassInvert = $('body').hasClass('invert');
+    let bodyEl = $('body');
+    let bodyHasClassBlue = bodyEl.hasClass('invert-blue');
+    let bodyHasClassInvert = bodyEl.hasClass('invert');
 
     if (bodyHasClassBlue) {
-        $('body').removeClass('invert-blue'); //убирает класс от синий инверсии
-        $('.spacial-panel-name').removeClass('spacial-panel-name_blue'); //убирает заголовки голубого цвет
-        $('#btn-standart').removeClass('spacial-panel-block-button__element_inverse_invert'); //убирает белую рамку стандартной кнопке
-        $('#btn-blue').removeClass('spacial-panel-btn-blue'); //убирает голубую рамку голубой кнопке
-        $('.spacial-panel-long-block-button').removeClass('spacial-panel-long-block-button_blue'); //убирает цвет полей на голубой
+        clearClass();
         newBtnArray.forEach(element => {
             if (element.classList.contains('invert-blue-active')) {
                 element.classList.remove("invert-blue-active");
                 element.classList.add("active");
             }
         });
-    } else if(bodyHasClassInvert) {
-        $('body').removeClass('invert'); //убирает класс от синий инверсии
-        $('#btn-standart').removeClass('spacial-panel-block-button__element_inverse_invert'); //убирает белую рамку стандартной кнопке
-        $('.spacial-panel-name').removeClass('spacial-panel-name_invert'); //убирает заголовки голубого цвет
-        $('#btn-standart').removeClass('spacial-panel-block-button__element_blue_inverse-blue'); //убирает белую рамку стандартной кнопке
-        $('.spacial-panel-long-block-button').removeClass('spacial-panel-long-block-button_invert'); //убирает цвет полей на голубой
+    } else if (bodyHasClassInvert) {
+        clearClass();
         newBtnArray.forEach(element => {
             if (element.classList.contains('invert-active')) {
                 element.classList.remove("invert-active");
@@ -36,7 +29,8 @@ function changeStandart() {
 function changeInverse() {
     let btnArray = document.getElementsByClassName('spacial-panel-long-block-button__element');
     let newBtnArray = [...btnArray];
-    let bodyHasClass = $('body').hasClass('invert-blue');
+    let bodyEl = $('body');
+    let bodyHasClass = bodyEl.hasClass('invert-blue');
     newBtnArray.forEach(element => {
         if (element.classList.contains('active')) {
             element.classList.remove("active");
@@ -53,18 +47,14 @@ function changeInverse() {
                 element.classList.add("invert-active");
             }
         });
-        $('body').removeClass('invert-blue'); //убирает класс от синий инверсии
-        $('.spacial-panel-name').removeClass('spacial-panel-name_blue'); //убирает заголовки голубого цвет
-        $('#btn-standart').removeClass('spacial-panel-block-button__element_inverse_invert'); //убирает белую рамку стандартной кнопке
-        $('#btn-blue').removeClass('spacial-panel-btn-blue'); //убирает голубую рамку голубой кнопке
-        $('.spacial-panel-long-block-button').removeClass('spacial-panel-long-block-button_blue'); //убирает цвет полей на голубой
+        clearClass();
     }
-    $('body').toggleClass('invert'); //изменяет общий цвет страницы
+    bodyEl.toggleClass('invert'); //изменяет общий цвет страницы
     $('.spacial-panel-name').toggleClass('spacial-panel-name_invert'); //изменяет заголовки на белый цвет
     $('#btn-standart').toggleClass('spacial-panel-block-button__element_inverse_invert'); //добавляет белую рамку стандартной кнопке
     $('#btn-blue').toggleClass('spacial-panel-block-button__element_blue_inverse-blue'); //добавляет голубую рамку голубой кнопке
     $('#btn-inverse').toggleClass('invert-active'); //изменяет цвет кнопки инверсии
-    $('.spacial-panel-regularversion__link').toggleClass('spacial-panel-regularversion__link_invert '); //изменяет цвет шрифта на белый
+    $('.spacial-panel-regularversion__link').toggleClass('spacial-panel-regularversion__link_invert'); //изменяет цвет шрифта на белый
     $('.spacial-panel-long-block-button').toggleClass('spacial-panel-long-block-button_invert'); //изменяет цвет полей на белый
 }
 
@@ -72,6 +62,9 @@ function changeInverse() {
 function changeBlue() {
     let btnArray = document.getElementsByClassName('spacial-panel-long-block-button__element');
     let newBtnArray = [...btnArray];
+    let bodyEl = $('body');
+    let bodyHasClass = bodyEl.hasClass('invert');
+
     newBtnArray.forEach(element => {
         if (element.classList.contains('active')) {
             element.classList.remove("active");
@@ -81,7 +74,6 @@ function changeBlue() {
             element.classList.add("active");
         }
     });
-    let bodyHasClass = $('body').hasClass('invert');
     if (bodyHasClass) {
         newBtnArray.forEach(element => {
             if (element.classList.contains('invert-active')) {
@@ -89,44 +81,95 @@ function changeBlue() {
                 element.classList.add('invert-blue-active');
             }
         })
-        $('body').removeClass('invert'); //убирает класс от синий инверсии
-        $('.spacial-panel-name').removeClass('spacial-panel-name_invert'); //убирает заголовки голубого цвет
-        $('#btn-standart').removeClass('spacial-panel-block-button__element_blue_inverse-blue'); //убирает белую рамку стандартной кнопке
-        $('.spacial-panel-long-block-button').removeClass('spacial-panel-long-block-button_invert'); //убирает цвет полей на голубой
+        clearClass();
     }
-    $('body').toggleClass('invert-blue'); //изменяет общий цвет страницы
+    bodyEl.toggleClass('invert-blue'); //изменяет общий цвет страницы
     $('.spacial-panel-name').toggleClass('spacial-panel-name_blue'); //изменяет заголовки на голубой цвет
     $('#btn-standart').toggleClass('spacial-panel-block-button__element_inverse_invert'); //добавляет белую рамку стандартной кнопке
+    $('#btn-inverse').toggleClass('spacial-panel-block-button__element_inverse_invert-blue')
     $('#btn-blue').toggleClass('spacial-panel-btn-blue'); //добавляет голубую рамку голубой кнопке
     $('.spacial-panel-long-block-button').toggleClass('spacial-panel-long-block-button_blue'); //изменяет цвет полей на голубой
+    $('a').toggleClass('spacial-panel-regularversion__link_blue'); //изменяет цвет полей на белый
+
 }
 
 //Размер шрифта
 function changeFontInSmall() {
+    let bodyEl = $('body');
     $('html').attr('data-font-size', 'small');
     $('#btn-ltr-spacing-normal').attr('data-text-ltr-spacing-standart', 'Стандартная');
-    $('#btn-font-small').addClass('active');
-    $('#btn-font-medium').removeClass('active');
-    $('#btn-font-large').removeClass('active');
+    if (bodyEl.hasClass('')) {
+        $('#btn-font-small').addClass('active');
+        $('#btn-font-medium').removeClass('active');
+        $('#btn-font-large').removeClass('active');
+    }
+    if (bodyEl.hasClass('invert')) {
+        $('#btn-font-small').addClass('invert-active');
+        $('#btn-font-medium').removeClass('invert-active');
+        $('#btn-font-large').removeClass('invert-active');
+    }
+    if (bodyEl.hasClass('invert-blue')) {
+        $('#btn-font-small').addClass('invert-blue-active');
+        $('#btn-font-medium').removeClass('invert-blue-active');
+        $('#btn-font-large').removeClass('invert-blue-active');
+    }
 }
-
 function changeFontInMedium() {
+    let bodyEl = $('body');
     $('html').attr('data-font-size', 'medium');
     $('#btn-standart').attr('data-text-palette-standart', 'Стандартная');
     $('#btn-ltr-spacing-normal').attr('data-text-ltr-spacing-standart', 'Стандарт.');
     $('#btn-lh-single').attr('data-text-lh-single', 'Одинарный');
-    $('#btn-font-small').removeClass('active');
-    $('#btn-font-medium').addClass('active');
-    $('#btn-font-large').removeClass('active');
+    if (bodyEl.hasClass('invert')) {
+        $('#btn-font-small').removeClass('invert-active');
+        $('#btn-font-medium').addClass('invert-active');
+        $('#btn-font-large').removeClass('invert-active');
+    }
+    if (bodyEl.hasClass('')) {
+        $('#btn-font-small').removeClass('active');
+        $('#btn-font-medium').addClass('active');
+        $('#btn-font-large').removeClass('active');
+    }
+    if (bodyEl.hasClass('invert-blue')) {
+        $('#btn-font-small').removeClass('invert-blue-active');
+        $('#btn-font-medium').addClass('invert-blue-active');
+        $('#btn-font-large').removeClass('invert-blue-active');
+    }
+
 }
 
 function changeFontInLarge() {
+    let bodyEl = $('body');
     $('html').attr('data-font-size', 'large');
-    $('#btn-standart').attr('data-text-palette-standart', 'Станд.');
+    $('#btn-standart').attr('data-text-palette-standart', 'Стандарт.');
     $('#btn-ltr-spacing-normal').attr('data-text-ltr-spacing-standart', 'Станд.');
+    $('#btn-lh-mid').attr('data-text-lh-mid', 'Полуторн.');
     $('#btn-lh-single').attr('data-text-lh-single', 'Одинар.');
-    $('#btn-font-small').removeClass('active');
-    $('#btn-font-medium').removeClass('active');
-    $('#btn-font-large').addClass('active');
+    $('.spacial-panel-container').css('grid-template-columns', '445px 360px 410px');
+    if (bodyEl.hasClass('')) {
+        $('#btn-font-small').removeClass('active');
+        $('#btn-font-medium').removeClass('active');
+        $('#btn-font-large').addClass('active');
+    }
+    if (bodyEl.hasClass('invert')) {
+        $('#btn-font-small').removeClass('invert-active');
+        $('#btn-font-medium').removeClass('invert-active');
+        $('#btn-font-large').addClass('invert-active');
+    }
+    if (bodyEl.hasClass('invert-blue')) {
+        $('#btn-font-small').removeClass('invert-blue-active');
+        $('#btn-font-medium').removeClass('invert-blue-active');
+        $('#btn-font-large').addClass('invert-blue-active');
+    }
+}
+
+function clearClass() {
+        $('body').removeClass().addClass('');
+        $('.spacial-panel-name').removeClass().addClass('spacial-panel-name');
+        $('.spacial-panel-long-block-button').removeClass().addClass('spacial-panel-long-block-button');
+        $('#btn-standart').removeClass().addClass('spacial-panel-block-button__element spacial-panel-block-button__element_standart');
+        $('#btn-inverse').removeClass().addClass('spacial-panel-block-button__element spacial-panel-block-button__element_inverse');
+        $('#btn-blue').removeClass().addClass('spacial-panel-block-button__element spacial-panel-block-button__element_blue');
+        $('a').removeClass().addClass('spacial-panel-regularversion__link');
 }
 
